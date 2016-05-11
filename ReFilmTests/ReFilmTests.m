c1a6b00d963957b18b654324a6c962a46d9614ee
VicChan <victorchanchina@gmail.com>
2016-05-11 20:05:26 +0800

    Initial Commit

---
+++ ReFilmTests/ReFilmTests.m
@@ -0,0 +1,39 @@
+//
+//  ReFilmTests.m
+//  ReFilmTests
+//
+//  Created by VicChan on 5/11/16.
+//  Copyright © 2016 VicChan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ReFilmTests : XCTestCase
+
+@end
+
+@implementation ReFilmTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
