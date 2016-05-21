eaed0c21bde32b331bacce7a6abbf3bc04cc2dd6
VicChan <victorchanchina@gmail.com>
2016-05-21 22:17:19 +0800

    线代稳过

--- ReFilm/AppDelegate.m
+++ ReFilm/AppDelegate.m
@@ -21,6 +21,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"线代稳过");
     _window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
 
     [[UINavigationBar appearance]setBarTintColor:[UIColor greenColor]];
