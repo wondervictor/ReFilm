7c2685b819f9ba585f4bddad19b02b9a4a21f3c2
VicChan <victorchanchina@gmail.com>
2016-05-22 14:59:03 +0800

    Handle HUD

--- ReFilm/RFHUD/RFProgressHUD.h
+++ ReFilm/RFHUD/RFProgressHUD.h
@@ -8,6 +8,41 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger,RFProgressHUDType) {
+    RFProgressHUDTypeDefault,
+    RFProgressHUDTypeError,
+    RFProgressHUDTypeSuccess
+};
+
+
 @interface RFProgressHUD : UIView
+
+
+@property (nonatomic, assign) CGFloat duration;
+
+@property (nonatomic, getter=isAnimating) BOOL animated;
+
+@property (nonatomic, copy) NSString *title;
+
+@property (nonatomic, weak) UIView *parentView;
+
+
+
+
+
+- (id)initWithFrame:(CGRect)frame;
+
+- (id)initWithSize:(CGSize)size center:(CGPoint)point duration:(CGFloat)duration toView:(UIView *__weak )view;
+
+/// 开始
+- (void)startAnimating;
+/// 结束
+- (void)stopAnimating;
+/// 成功
+- (void)stopWithSuccess:(NSString *)succesMsg;
+/// 错误
+- (void)stopWithError:(NSString *)errorMsg;
+/// 
+- (void)startAnimatingWithTitle:(NSString *)title;
 
 @end
--- ReFilm/RFHUD/RFProgressHUD.m
+++ ReFilm/RFHUD/RFProgressHUD.m
@@ -7,12 +7,60 @@
 //
 
 #import "RFProgressHUD.h"
-
+#define VIEW_WIDTH   (self.frame.size.width)
+#define VIEW_HEIGHT  (self.frame.size.height)
 
 @interface RFProgressHUD()
 
 @end
 
 @implementation RFProgressHUD
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // default Configuration
+        _duration = 0.5f;
+        _animated = NO;
+        self.layer.cornerRadius = 5.0f;
+        
+    }
+    return self;
+}
+
+- (id)initWithSize:(CGSize)size center:(CGPoint)point duration:(CGFloat)duration toView:(UIView *__weak)view {
+    
+    self = [super initWithFrame:CGRectMake(point.x-size.width/2.0, point.y-size.height/2, size.width, size.height)];
+    if (self) {
+        _duration = duration;
+        _parentView = view;
+        _animated = NO;
+        
+        
+    }
+    return self;
+}
+
+- (void)startAnimating {
+    _animated = YES;
+}
+
+
+- (void)stopAnimating {
+    _animated = NO;
+}
+
+- (void)stopWithError:(NSString *)errorMsg {
+    
+}
+
+- (void)stopWithSuccess:(NSString *)succesMsg {
+    
+}
+
+
+
+
+
 
 @end
